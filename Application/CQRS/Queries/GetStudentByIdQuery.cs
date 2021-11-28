f21d257d49ec666cc92bd37c44f328a74e0ef4fd
Mohamed Badr <Mohamed.Badr@integrant.com>
2021-11-28 11:23:19 +0200

    Add project files.

---
+++ Application/CQRS/Queries/GetStudentByIdQuery.cs
@@ -0,0 +1,31 @@
+﻿using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.CQRS.Queries
+{
+    public class GetStudentByIdQuery : IRequest<Student>
+    {
+        public int Id { get; set; }
+        public class GetStudentByIdQueryHandler : IRequestHandler<GetStudentByIdQuery, Student>
+        {
+            private readonly IAppDbContext context;
+            public GetStudentByIdQueryHandler(IAppDbContext context)
+            {
+                this.context = context;
+            }
+            public async Task<Student> Handle(GetStudentByIdQuery query, CancellationToken cancellationToken)
+            {
+                var student = await context.Students.Where(a => a.Id == query.Id).FirstOrDefaultAsync();
+                if (student == null) return null;
+                return student;
+            }
+        }
+    }
+}
---
+++ Domain/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Standard { get; set; }
+        public int Rank { get; set; }
+    }
+}
